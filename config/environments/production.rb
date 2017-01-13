a017ba4cd208888b56037a3578310b143cf176ed
Steph, Toma, Max <stephandtomaandmax@LEARNacademy.com>
2017-01-13 13:44:15 -0800

    updated ENV.fetch for S3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,10 +89,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('apartment-app-images'),
-      access_key_id: ENV.fetch('S3_KEY'),
-      secret_access_key: ENV.fetch('S3_SECRET'),
-      s3_region: ENV.fetch('us-west-1'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 end
