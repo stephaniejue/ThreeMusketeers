f6c9f75b0164d648d9414dd8bfba946712730cc8
Steph, Toma, Max <stephandtomaandmax@LEARNacademy.com>
2017-01-13 14:11:09 -0800

    Removed S3 url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,6 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-    :url => ":s3_host_name"
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   }
 end
