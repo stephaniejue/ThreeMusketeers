73a0ed6c5b392492bb338dc33c77fe30261e73cb
Stephanie Chin <stephjuechin@gmail.com>
2017-02-02 14:32:57 -0800

    Added new rubocop todo

--- app/assets/javascripts/apartments.js
+++ app/assets/javascripts/apartments.js
@@ -1,19 +1,20 @@
-function createGmap(data) {
-  // console.log("createGmap running");
-  handler = Gmaps.build('Google');
-  handler.buildMap ({
-      provider: {},
-      internal: {id: 'apartment_map'}
-    },
-    function() {
-      markers = handler.addMarkers(data);
-      handler.bounds.extendWith(markers);
-      handler.fitMapToBounds();
-      // handler.getMap().setZoom(7);
-    }
-  );
+function loadAndCreateGmap() {
+  // console.log("loadAndCreateGmap running");
+  if ($("#apartment_map").length > 0) {
+    var apartmentId = $("#apartment_map").attr("data-apartment-id");
+    $.ajax({
+      dataType: 'json',
+      url: '/apartments/' + apartmentId + '/map_location',
+      method: 'GET',
+      success: function(data) {
+        placeMarkers(data);
+      },
+      error: function (jqXHR, testStatus, errorThrown) {
+        alert("Getting map data failed: " + errorThrown);
+      }
+    });
+  }
 };
-
 
 function placeMarkers(data) {
   // console.log("placeMarkers running");
@@ -39,22 +40,25 @@ function placeMarkers(data) {
   }
 }
 
-function loadAndCreateGmap() {
-  // console.log("loadAndCreateGmap running");
-  if ($("#apartment_map").length > 0) {
-    var apartmentId = $("#apartment_map").attr("data-apartment-id");
-    $.ajax({
-      dataType: 'json',
-      url: '/apartments/' + apartmentId + '/map_location',
-      method: 'GET',
-      success: function(data) {
-        placeMarkers(data);
-      },
-      error: function (jqXHR, testStatus, errorThrown) {
-        alert("Getting map data failed: " + errorThrown);
-      }
-    });
-  }
+function createGmap(data) {
+  // console.log("createGmap running");
+  handler = Gmaps.build('Google');
+  handler.buildMap ({
+      provider: {},
+      internal: {id: 'apartment_map'}
+    },
+    function() {
+      markers = handler.addMarkers(data);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+      // handler.getMap().setZoom(7);
+    }
+  );
 };
+
+
+
+
+
 $(document).on('ready', loadAndCreateGmap);
 $(document).on('turbolinks:load', loadAndCreateGmap);
